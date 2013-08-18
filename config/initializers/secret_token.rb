b862cff8c6394a3c59e23066314b7729bfea0c5a
Tamaki <tmaeda@clustium.com>
2013-08-19 06:24:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benesse::Application.config.secret_token = 'eea2b2ca9a08445f9b7498679a1f8c64a535d7172982c3b62073f118b61ae698ee4ac030b41e5d425d25f239cbc270c82c8b989804f7a276abfc3805d782a2a5'
