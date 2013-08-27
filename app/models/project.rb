43c996a527401c71b1d3974fdf7ffbe46955d690
Ichiro Kimura <kag.web@gmail.com>
2013-08-28 03:16:16 +0900

    デジタル推進課かどうかの判定追加&projectのseedデータ作成

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,20 @@
+# coding: utf-8
 class Project < ActiveRecord::Base
   has_many :branches
   has_many :comments
   has_many :confirmations
   has_many :parties
-  has_one :authorizer, class_name: 'User'
-  has_one :promoter, class_name: 'User'
-  has_one :operator, class_name: 'User'
+  belongs_to :authorizer, class_name: 'User'
+  belongs_to :promoter, class_name: 'User'
+  belongs_to :operator, class_name: 'User'
 
   attr_accessible :authorizer_id, :code, :confirmed, :name, :production_upload_at, :production_upload_url, :promoter_id, :status, :test_upload_at, :upload_url
+
+  after_create :create_branche
+
+  private
+
+  def create_branche
+    branches.create code: (format("%07d", id) + '01') # テストしてない
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
 if Rails.env.development?
   ActiveRecord::Base.connection.tables.map { |table| Object.const_get(table.classify) rescue nil }.compact.each do |_class|
     _class.delete_all
@@ -16,6 +9,9 @@ if Rails.env.development?
   end
 
   department = Department.find_by_name('デジタル推進課')
-  user = department.users.create username: 'user', email: 'admin@mail.benesse.co.jp', password: 'secret', password_confirmation: 'secret'
+  user = department.users.create username: 'user', name: 'ベネッセ太郎', email: 'admin@mail.benesse.co.jp', password: 'secret', password_confirmation: 'secret'
 
+  100.times { |i|
+    Project.create name: "プロジェクト#{i}", promoter_id: user.id
+  }
 end
