cb621ccda3f89207562f3d689f5af58f865ad57c
Tamaki <tmaeda@clustium.com>
2013-08-28 11:19:40 +0900

    移送案件個票ページのマークアップ

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,9 @@ class Project < ActiveRecord::Base
   has_many :comments
   has_many :confirmations
   has_many :parties
-  has_one :authorizer, class_name: 'User'
-  has_one :promoter, class_name: 'User'
-  has_one :operator, class_name: 'User'
+  belongs_to :authorizer, class_name: 'User'
+  belongs_to :promoter, class_name: 'User'
+  belongs_to :operator, class_name: 'User'
 
   attr_accessible :authorizer_id, :code, :confirmed, :name, :production_upload_at, :production_upload_url, :promoter_id, :status, :test_upload_at, :upload_url
 end
