4867609065309c9307fd478c6dff1a9a75d2dbe5
Tamaki <tmaeda@clustium.com>
2013-09-01 02:12:35 +0900

    Parties 用のテストデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,41 @@ if Rails.env.development?
     _class.delete_all
   end
 
-  ['デジタル推進課', '部署1', '部署2'].each do |name|
-    Department.create name: name
+  users = []
+
+  ['デジタル推進課', '部署1', '部署2', '部署3'].each do |name|
+    department = Department.create name: name
+
+    20.times do |i|
+      users << department.users.create(username: "user#{department.id}_#{i}", name: "テストユーザ#{i}", email: "user#{department.id}_#{i}@mail.benesse.co.jp", password: 'secret', password_confirmation: 'secret')
+    end
   end
 
-  department = Department.find_by_name('デジタル推進課')
-  user = department.users.create username: 'user', name: 'ベネッセ太郎', email: 'admin@mail.benesse.co.jp', password: 'secret', password_confirmation: 'secret'
 
-  100.times { |i|
-    Project.create name: "プロジェクト#{i}", promoter_id: user.id
-  }
+  department = Department.find_by_name('デジタル推進課')
+
+  # 開発用ログインユーザ
+  user = department.users.create(username: 'user', name: 'ベネッセ太郎', email: 'admin@mail.benesse.co.jp', password: 'secret', password_confirmation: 'secret')
+
+  projects = []
+
+  100.times do |i|
+    tmp = Project.new(name: "プロジェクト#{i}")
+    tmp.authorizer = user
+    tmp.promoter = user
+    tmp.operator = user
+    tmp.save
+    projects << tmp
+  end
+
+  projects.each do |project|
+    (rand 5..10).times do |i|
+      user = users[rand(users.length)]
+      party = project.parties.new(required: rand(2) == 1)
+      party.project = project
+      party.user = user
+      party.save
+    end
+  end
+
 end
