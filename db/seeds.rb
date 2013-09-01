e7fa5dc94d1a19efec6dabdf155b1311b77eefef
Tamaki <tmaeda@clustium.com>
2013-09-02 05:09:14 +0900

    各案件にコメントを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@ if Rails.env.development?
   ['デジタル推進課', '部署1', '部署2', '部署3'].each do |name|
     department = Department.create name: name
 
+    # 各プロジェクトの関係者として利用するテストユーザ
     20.times do |i|
       users << department.users.create(username: "user#{department.id}_#{i}", name: "テストユーザ#{i}", email: "user#{department.id}_#{i}@mail.benesse.co.jp", password: 'secret', password_confirmation: 'secret')
     end
@@ -32,16 +33,30 @@ if Rails.env.development?
   end
 
   projects.each do |project|
+    parties = []
+    # 開発用ログインユーザを各プロジェクトの関係者にアサイン
     party = project.parties.new(required: true)
     party.project = project
     party.user = user
     party.save
+    parties << user
 
+    # 関係者を5~10人適当に登録
     (rand 5..10).times do |i|
       party = project.parties.new(required: rand(2) == 1)
       party.project = project
       party.user = users[rand(users.length)]
       party.save
+      parties << party.user
+    end
+
+    # コメントを登録
+    (rand 10..20).times do |i|
+      0..3.times do |status|
+        comment = project.comments.new status: status, comment: 'コメントのテストです。'
+        comment.user = parties[rand(parties.length)]
+        comment.save
+      end
     end
   end
 
