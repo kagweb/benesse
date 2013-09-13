394772d38519abb730e3ff04b02288bad0800070
Tamaki <tmaeda@clustium.com>
2013-09-13 16:42:10 +0900

    ファイルビューアのリスト表示の一部実装

--- app/assets/javascripts/projects.js.coffee
+++ app/assets/javascripts/projects.js.coffee
@@ -7,4 +7,13 @@ $ ->
     $('.selected').removeClass 'selected'
     $(this).parent('li').addClass 'selected'
     $('input[type="text"][name="path"]').val $(this).attr('href')
+
+    # List 表示画面へのリンクのパラメータを書き換え
+    href = $('a.list').attr('href').split '?'
+    old_params = href[1].split '&'
+    new_params = ''
+    for o in old_params
+      new_params += o + '&' unless o.match /^path\=/
+    new_params += 'path=' + encodeURIComponent($(this).attr 'href')
+    $('a.list').attr 'href',  href[0] + '?' + new_params
     return false
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -39,12 +39,70 @@ module ProjectsHelper
     return confirmation.empty? ? '' : confirmation.last.response
   end
 
-  def break_dir ( name, info )
-    return '' if name == '_path_'
-    return "<li><a href=\"#{info}\"><i class=\"icon-file\"></i> #{name}</a></li>" if info.instance_of? String
+  def folder ( name, info )
+    # ファイルの情報
+    tmp  = "<div class=\"pull-right span2\">#{info['_updated_at_']} </div>"
+    tmp += "<div class=\"pull-right span1\">#{info['_size_']}</div>"
+    tmp += "<div class=\"pull-right span1\">#{info['_type_'].upcase}</div>"
+
+    # ディレクトリでない場合は li 要素を返す
+    return "<li class=\"file\"><a href=\"#{info['_path_']}\"><i class=\"icon-file\"></i> #{name} #{tmp} </a></li>" unless info['_type_'] == 'dir'
+
+    # ディレクトリの場合はディレクトリの中身を再帰的に解析
     element = "<li><a href=\"#{info['_path_']}\"><i class=\"icon-folder-open\"></i> #{name}</a><ul class=\"unstyled\">"
-    info['_files_'].each {|n, i| element += break_dir n, i }
+    info['_files_'].each {|n, i| element += folder n, i }
 
     return element + "</ul></li>"
+  end
+
+  def list_path
+    return false if params[:path].blank?
+    path = Rails.root.join URI.unescape params[:path]
+    return false unless path.exist?
+    return path
+  end
+
+  def get_dir_structure(path, depth = nil)
+    require "find"
+
+    path = Rails.root.join path
+    return false unless path.exist?
+
+    dir = {}
+
+    Find.find path do |f|
+      resolved_path = f.to_s.gsub(Rails.root.to_s, '').gsub(/^\//, '').split /\//
+      next if resolved_path.empty?
+
+      tmp = dir
+      count = 0
+
+      resolved_path.each do |r|
+        count += 1
+
+        if FileTest.file? f and count == resolved_path.length
+          tmp[r] = {
+            '_path_' => resolved_path.join('/'),
+            '_type_' => r.split('.').last.upcase,
+            '_size_' => number_to_human_size(File.size? f) || 'empty',
+            '_updated_at_' => l(File.mtime f),
+          }
+          break
+        elsif ! tmp.key? r
+          tmp[r] = {
+            '_path_' => resolved_path.join('/'),
+            '_type_' => 'dir',
+            '_size_' => number_to_human_size(File.size? f ) || 'empty',
+            '_updated_at_' => l(File.mtime f),
+            '_files_' => {},
+          }
+        end
+
+        tmp = tmp[r]['_files_']
+        break if ! depth.nil? and count >= depth
+      end
+    end
+
+    return dir
   end
 end
