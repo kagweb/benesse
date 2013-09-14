d8c368ffb2dadee98aadd8eb0b35403fffdb4bac
Tamaki <tmaeda@clustium.com>
2013-09-15 07:03:31 +0900

    日本語化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new params[:user]
     if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
+      redirect_to projects_path, notice: 'User was successfully created.'
     else
       render :new
     end
