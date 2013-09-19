9ef56af8ac7c2fe5ed55204baa2290cbe1f34e1e
Tamaki <tmaeda@clustium.com>
2013-09-19 23:58:33 +0900

    業者を案件の承認者・担当者・更新作業担当者・関係者に登録できないように修整

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,12 @@
+# encoding: utf-8
 class ApiController < ApplicationController
   def user_list
-    render json: params[:department].empty? ? User.all : Department.find(params[:department]).try(:users)
+    render json: params[:department].blank? ? User.where(['department_id != ?', _department_id]) : User.where(department_id: _department_id)
+  end
+
+  private
+
+  def _department_id
+    return params[:department].presence || Department.find_by_name('業者').id
   end
 end
