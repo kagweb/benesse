62410b2463945ad26dbb3ba50e38783b96338700
Tamaki <tmaeda@clustium.com>
2013-09-19 20:18:07 +0900

    進捗ステータスの更新タイミングを調整

--- app/models/confirmation.rb
+++ app/models/confirmation.rb
@@ -9,15 +9,28 @@ class Confirmation < ActiveRecord::Base
   private
 
   def check_status
+    return false unless [2,4,6].include? project.status
+
     updated = true
+
     project.parties.each do |party|
       next unless party.required
-      updated = false unless 'ok' == party.user.confirmations.find_by_status(project.status_code).try(:response)
+      updated = false unless 'ok' == party.user.confirmations.find_by_status(status).try(:response)
     end
 
+    pp project.status
     if updated
-      project.status += 1
+      case project.status
+        when 2
+          project.status = 3
+        when 4
+          project.status = 5
+        when 6
+          project.status = 7
+      end
       project.save
     end
+
+    pp project.status
   end
 end
