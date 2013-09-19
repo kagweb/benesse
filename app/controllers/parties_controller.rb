9ddc5f4c3ecc19195ac0ebaad6153971e2bd3aa2
Tamaki <tmaeda@clustium.com>
2013-09-20 05:34:23 +0900

    関係者の登録情報の編集を可能に。

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 class PartiesController < ApplicationController
   before_filter :require_login
   before_filter :supplier_department_except
@@ -27,7 +28,7 @@ class PartiesController < ApplicationController
     # Party.where('project_id' => params[:party][:project_id], 'user_id' => params[:party][:user])
 
     if @party.save
-      redirect_to @party.project, notice: 'Party was successfully created.'
+      redirect_to @party.project, notice: '関係者の追加が完了しました。'
     else
       render :new
     end
@@ -36,7 +37,7 @@ class PartiesController < ApplicationController
   def update
     @party = Party.find params[:id]
     if @party.update_attributes params[:party]
-      redirect_to @party, notice: 'Party was successfully updated.'
+      redirect_to @party.project, notice: '関係者の登録情報の編集が完了しました。'
     else
       render :edit
     end
