decbe9ed9e878577a736b78d0623396f6f8edae0
Tamaki <tmaeda@clustium.com>
2013-10-08 16:14:31 +0900

    routes をリファクタリング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,51 @@
 Benesse::Application.routes.draw do
+  root to: 'projects#index'
+
+  # リファクタリング後
+#   namespace :api do
+#     resources :users, only: :index
+#     resources :projects, only: :index do
+#       get :close
+#       resources :files, only: :index
+#     end
+#   end
+#
+#   resources :projects do
+#     get :send_mail, on: :member
+#
+#     resources :change_users
+#     resources :checkups do
+#       put :confirmation
+#     end
+#     resources :files
+#     resouces :comments
+#     resources :confirms, only: [] do
+#       collection do
+#         put :authority
+#         put :aws
+#         put :project
+#         put :update_branch
+#         put :miss
+#         put :aws_reset
+#       end
+#     end
+#     resources :parties, except: [:index, :show]
+#   end
+#
+#   resources :departments, except: [:show]
+#   resources :users, except: [:show]
+#   resources :sessions, only: [:new, :create, :destroy]
+#   resources :aws, only: [:index, :create, :update, :destroy]
+#
+#   match 'login' => 'sessions#new', as: :login
+#   match 'logout' => 'sessions#destroy', as: :logout
+
+
+  # リファクタリング前
   get "api/user_list" => 'api#user_list'
   get "api/projects" => 'api#projects'
   get "downloads" => 'downloads#optional'
   post "downloads/get_url" => 'downloads#get_url'
-
-  root to: 'projects#index'
 
   resources :projects do
     member do
@@ -58,61 +99,4 @@ Benesse::Application.routes.draw do
 
   match 'login' => 'sessions#new', as: :login
   match 'logout' => 'sessions#destroy', as: :logout
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
